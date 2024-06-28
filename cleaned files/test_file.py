c5dc794cbdfd33ccefcfd312ffc509ab657f0ba4
Phousawanh Peaungvongpakdy <ppeaungvongpakdy@g.hmc.edu>
2024-06-28 09:05:02 -0400

    Added test file

---
+++ cleaned files/test_file.py
@@ -0,0 +1,16 @@
+"""
+Author          : Phousawanh
+Last edited     : 2024-06-27
+Description     : Simulation Experiments    
+"""
+
+import seaborn as sns
+
+from scipy.stats import kurtosis, skew
+
+from rcv_distribution import *
+from MDS_analysis import *
+from voting_rules import *
+from consistency import *
+
+print("Hello World!")
